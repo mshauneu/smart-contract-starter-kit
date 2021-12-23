f751a9578b459850a95833981d04792ff6b4ff6e
Mike Shauneu <m.shauneu@gmail.com>
2021-12-22 23:30:03 -0500

    Polishing...

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,9 +1,8 @@
 import '@nomiclabs/hardhat-waffle';
 import { task } from 'hardhat/config';
-import { Signer } from '@ethersproject/abstract-signer';
 
 task('accounts', 'Prints the list of accounts')
   .setAction(async (_ta, { ethers }) => {
-    const accounts: Signer[] = await ethers.getSigners();
+    const accounts = await ethers.getSigners();
     console.log(await Promise.all(accounts.map((a) => a.getAddress)));
   });
